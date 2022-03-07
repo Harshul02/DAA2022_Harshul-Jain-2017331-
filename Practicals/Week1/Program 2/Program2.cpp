1828eeafa71b6ef054cccf1c5fe385ce8b32ac67
Harshul Jain <harshulgangwal02@gmail.com>
2022-03-07 18:25:56 +0530

    Added another condition

--- Practicals/Week1/Program 2/Program2.cpp
+++ Practicals/Week1/Program 2/Program2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     int c=0,r=0;
+    //int x=0;
     int n,f;
     cout<<"Enter the size of the array: ";
     cin>>n;
@@ -25,25 +26,32 @@ int main()
     if(f==a[s-1])
     {
         cout<<"Key found at location : "<<s<<endl;
-        cout<<"No of iteration : "<<1<<endl;
+        cout<<"No of operation : "<<1<<endl;
     }
     else if(f<a[s-1])
     {
         for(int i=0; i<s-1; i++)
         {
-            if(a[i]!=f)
+            if(a[i]!=f && f<a[i])
+            {
+                c++;
+                cout<<"Key not found"<<endl;
+                cout<<"No. of operations: "<<c<<endl;
+                break;
+            }
+            else if(a[i]!=f)
             {
                 c++;
             }
-            else{
+            else if(a[i]==f){
                 cout<<"Key found at location : "<<i+1<<endl;
-                cout<<"No of iteration : "<<i+1<<endl;
+                cout<<"No of operations : "<<i+1<<endl;
                 break;
             }
             if(c==s-1)
             {
                 cout<<"Key not Found"<<endl;
-                cout<<"No of iteration: "<<c<<endl;
+                cout<<"No of operations: "<<c<<endl;
             }
         }
     }
@@ -51,19 +59,26 @@ int main()
     {
         for(int i=s; i<n; i++)
         {
-            if(a[i]!=f)
+            if(a[i]!=f && f<a[i])
+            {
+                r++;
+                cout<<"Key not found"<<endl;
+                cout<<"No. of operations: "<<r<<endl;
+                break;
+            }
+            else if(a[i]!=f)
             {
                 r++;
             }
-            else{
+            else if(a[i]==f){
                 cout<<"Key found at location : "<<i+1<<endl;
-                cout<<"No of iteration : "<<r+1<<endl;
+                cout<<"No of operations : "<<r+1<<endl;
                 break;
             }
             if(r==s-1)
             {
                 cout<<"Key not Found"<<endl;
-                cout<<"No of iteration: "<<r<<endl;
+                cout<<"No of operations: "<<r<<endl;
             }
     }
     }
