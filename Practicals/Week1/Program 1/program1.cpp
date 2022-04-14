437791f98040a51021005c20a54aa6dbe26d397a
Harshul Jain <harshulgangwal02@gmail.com>
2022-04-14 19:15:27 +0530

    Week 2

--- Practicals/Week1/Program 1/program1.cpp
+++ Practicals/Week1/Program 1/program1.cpp
@@ -3,33 +3,32 @@ using namespace std;
 
 int main()
 {
-    int n,f;
-    cout<<"Enter the size of the array: ";
-    cin>>n;
-    int a[n];
-    cout<<endl<<"Input the array"<<endl;
-    for(int i=0; i<n; i++)
+    int t;
+    cin>>t;
+    while(t--)
     {
-        cin>>a[i];
-    }
-    cout<<endl<<"Enter the key to find: ";
-    cin>>f;
-    int c=0;
-    for(int i=0; i<n; i++)
-    {
-        if(a[i]!=f)
-            c++;
-        else{
-            cout<<"Element found on position : "<<i+1<<endl;
-            cout<<"No of iterations : "<<c+1<<endl;
-            break;
+        int n,f;
+        cin>>n;
+        int a[n];
+        for(int i=0; i<n; i++)
+        {
+            cin>>a[i];
+        }
+        cin>>f;
+        int c=0;
+        for(int i=0; i<n; i++)
+        {
+            if(a[i]!=f)
+                c++;
+            else{
+                cout<<"Present "<<i+1<<endl;
+                break;
+            }
+        }
+        if(c==n)
+        {
+            cout<<"Not Present "<<c<<endl;
         }
     }
-    if(c==n)
-    {
-        cout<<"Element not found"<<endl;
-        cout<<"No of iterations : "<<c<<endl;
-    }
-
     return 0;
 }
--- Practicals/Week1/Program 2/Program2.cpp
+++ Practicals/Week1/Program 2/Program2.cpp
@@ -3,84 +3,82 @@ using namespace std;
 
 int main()
 {
-    int c=0,r=0;
-    //int x=0;
-    int n,f;
-    cout<<"Enter the size of the array: ";
-    cin>>n;
-    int a[n];
-    cout<<endl<<"Input the array"<<endl;
-    for(int i=0; i<n; i++)
+    int t;
+    cin>>t;
+    while(t--)
     {
-        cin>>a[i];
-    }
-    cout<<endl<<"Enter the key to find: ";
-    cin>>f;
-    cout<<endl;
-    int s=0;
-    if(n%2==0)
-        s=n/2;
-    else
-        s=n/2+1;
-
-    if(f==a[s-1])
-    {
-        cout<<"Key found at location : "<<s<<endl;
-        cout<<"No of operation : "<<1<<endl;
-    }
-    else if(f<a[s-1])
-    {
-        for(int i=0; i<s-1; i++)
+        int c=0,r=0;
+        int n,f;
+        cin>>n;
+        int a[n];
+        for(int i=0; i<n; i++)
         {
-            if(a[i]!=f && f<a[i])
+            cin>>a[i];
+        }
+        cin>>f;
+        cout<<endl;
+        int s=0;
+        s=n/2;
+
+        if(f==a[s])
+        {
+            cout<<"Present"<<endl;
+            cout<<"No of comparisions: "<<1<<endl;
+        }
+        else if(f<a[s])
+        {
+            for(int i=0; i<s; i++)
             {
-                c++;
-                cout<<"Key not found"<<endl;
-                cout<<"No. of operations: "<<c<<endl;
-                break;
-            }
-            else if(a[i]!=f)
-            {
-                c++;
-            }
-            else if(a[i]==f){
-                cout<<"Key found at location : "<<i+1<<endl;
-                cout<<"No of operations : "<<i+1<<endl;
-                break;
-            }
-            if(c==s-1)
-            {
-                cout<<"Key not Found"<<endl;
-                cout<<"No of operations: "<<c<<endl;
+                if(a[i]!=f && f<a[i])
+                {
+                    c++;
+                    cout<<"Not Present"<<endl;
+                    cout<<"No. of comparisions: "<<c+1<<endl;
+                    break;
+                }
+                else if(a[i]!=f)
+                {
+                    c++;
+                }
+                else if(a[i]==f){
+                    cout<<"Present"<<endl;
+                    cout<<"No of comparisions: "<<c+2<<endl;
+                    break;
+                }
+                if(c==s)
+                {
+                    cout<<"Not Present"<<endl;
+                    cout<<"No of comparisions: "<<c+1<<endl;
+                }
             }
         }
-    }
-    else
-    {
-        for(int i=s; i<n; i++)
+        else
         {
-            if(a[i]!=f && f<a[i])
+            for(int i=s+1; i<n; i++)
             {
-                r++;
-                cout<<"Key not found"<<endl;
-                cout<<"No. of operations: "<<r<<endl;
-                break;
-            }
-            else if(a[i]!=f)
-            {
-                r++;
-            }
-            else if(a[i]==f){
-                cout<<"Key found at location : "<<i+1<<endl;
-                cout<<"No of operations : "<<r+1<<endl;
-                break;
-            }
-            if(r==s-1)
-            {
-                cout<<"Key not Found"<<endl;
-                cout<<"No of operations: "<<r<<endl;
-            }
-    }
+                if(a[i]!=f && f<a[i])
+                {
+                    r++;
+                    cout<<"Not Present"<<endl;
+                    cout<<"No. of comparisions: "<<r+1<<endl;
+                    break;
+                }
+                else if(a[i]!=f)
+                {
+                    r++;
+                }
+                else if(a[i]==f){
+                    cout<<"Present"<<endl;
+                    cout<<"No of comparisions: "<<r+2<<endl;
+                    break;
+                }
+                if(r==s)
+                {
+                    cout<<"Not Present"<<endl;
+                    cout<<"No of comparisions: "<<r+1<<endl;
+                }
+        }
+        }
     }
     return 0;
 }
